820eb92df1fcbc890679b01f8064d4609b357657
Aalphakeem-Adroit <alphakeem12@gmail.com>
2025-07-03 22:58:09 +0100

    Fix: Js for mobile nav not loading

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,3 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // Getting current year for footer
-  document.getElementById('year').textContent = new Date().getFullYear();
-
-  // Open Side Nav
-  function openNav() {
-    document.getElementById("mobileScreenMenu").style.width = "250px";
-  }
-
-  // Close Side Nav
-  function closeNav() {
-    document.getElementById("mobileScreenMenu").style.width = "0";
-  }
-});
+// document.addEventListener("DOMContentLoaded", () => {
+  
+// });
