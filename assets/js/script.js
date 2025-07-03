6663b8f6855504b679759012203dc51464822d78
Aalphakeem-Adroit <alphakeem12@gmail.com>
2025-07-03 22:46:34 +0100

    Fix: Sidenav not opening

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,14 @@
-import { SpeedInsights } from "@vercel/speed-insights/next"
+document.addEventListener("DOMContentLoaded", () => {
+  // Getting current year for footer
+  document.getElementById('year').textContent = new Date().getFullYear();
 
-// Getting current year for footer
-document.getElementById('year').textContent = new Date().getFullYear();
+  // Open Side Nav
+  function openNav() {
+    document.getElementById("mobileScreenMenu").style.width = "250px";
+  }
 
-// Open Side Nav
-function openNav() {
-  document.getElementById("mobileScreenMenu").style.width = "250px";
-}
-
-// Close Side Nav
-function closeNav() {
-  document.getElementById("mobileScreenMenu").style.width = "0";
-}
+  // Close Side Nav
+  function closeNav() {
+    document.getElementById("mobileScreenMenu").style.width = "0";
+  }
+});
